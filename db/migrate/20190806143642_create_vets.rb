6baeb7864c30902669c94c98838758716684dbb8
Irma Mejia <ijanettmejia@outlook.com>
2019-08-06 10:37:43 -0400

    Generate vets resources

---
+++ db/migrate/20190806143642_create_vets.rb
@@ -0,0 +1,12 @@
+class CreateVets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :vets do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
