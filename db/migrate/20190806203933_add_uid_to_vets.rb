7a1cda87544fdeb9f4fff93884613bd3a5b90f5e
Irma Mejia <ijanettmejia@outlook.com>
2019-08-06 18:12:01 -0400

    Seed database with dummy data for clinics

--- db/migrate/20190806203933_add_uid_to_vets.rb
+++
@@ -1,5 +0,0 @@
-class AddUidToVets < ActiveRecord::Migration[5.2]
-  def change
-    add_column :vets, :uid, :integer
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+clinics = [
+    {name: "Queenstowne Cat Clinic", location: "Alexandria, Virginia", description: "We're commited to delivering quality medical and surgical care for your cats."},
+    {name: "Brooks Animal Clinic", location: "Fairfax, Virginia", description: "Our veterinarians and staff are dedicated to your pet's health."},
+    {name: "Dales City Clinic", location: "Dales City, Virginia", description: "We care for you and your companion and will work with you to help you understand your pet's health needs."},
+    {name: "Parkway Animal Clinic", location: "Fairfax, Virginia", description: "Providing comprehensive medical, surgical, and dental care for our pet-patients since 1980."},
+    {name: "Paradise Animal Clinic", location: "Gardenstowne, Virginia", description: "A warm and inviting atmosphere combined with the latest in veterinary medicine and surgery, Petfield Clinic offers an exceptional concept in animal care."},
+    {name: "Clinic of Internal Medicine", location: "Burke, Virginia", description: "Our board certified internal medicine vets have an extensive background in evaluation, diagnosis and treatment of internal medicine conditions in cats and dogs"},
+    {name: "Wags n Kisses Dog Clinic", location: "Alexandria, Virginia", description: "We are dedicated to delivering the highest possible service to ensure the quality of life and well being of your dog."},
+]
+
+clinics.each do |clinic|
+    Clinic.create(clinic)
+end
