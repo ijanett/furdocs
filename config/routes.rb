ed312056113587eab67a0e90d1fd3c449e636900
Irma Mejia <ijanettmejia@outlook.com>
2019-08-06 16:29:31 -0400

    Add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resources :pets
   resources :owners
 
-  get '/signin', to: 'sessions#new'
-  post '/signin', to: 'sessions#create'
+  get '/login', to: 'sessions#new'
+  get '/auth/github/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
