62bd45571e5af4aaadf5bf7f66f4179531b860dc
Irma Mejia <ijanettmejia@outlook.com>
2019-08-07 19:11:46 -0400

    Add validations to vet model

--- app/models/vet.rb
+++ app/models/vet.rb
@@ -4,12 +4,15 @@ class Vet < ApplicationRecord
     has_many :charts
     belongs_to :clinic
     has_secure_password
+    
+    validates :email, presence: true, uniqueness: true, format: { with: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i }
+    validates :password, presence: true, confirmation: true, length: { minimum: 6 }
 
     def full_name
-        self.first_name + " " + self.last_name
+        self.first_name.capitalize + " " + self.last_name.capitalize
     end
 
     def full_dr_name
-        "Dr. " + self.first_name + " " + self.last_name
+        "Dr. " + self.full_name
     end
 end
