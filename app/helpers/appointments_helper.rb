381181ebe4252d1949a94f711d716c2e79e1ee91
Irma Mejia <ijanettmejia@outlook.com>
2019-08-08 12:40:23 -0400

    Add appointment helper methods to display date and time

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,2 +1,10 @@
 module AppointmentsHelper
+
+    def appt_date(appt)
+        appt.date.strftime("%b %d, %Y")
+    end
+    
+    def appt_time(appt)
+        appt.time.strftime("%l:%M %p")
+    end
 end
