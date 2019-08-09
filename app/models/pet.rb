cad05d72e30d201e8bd7753603303fa7678e5f65
Irma Mejia <ijanettmejia@outlook.com>
2019-08-09 18:27:55 -0400

    Add method in pet model to calculate age in months

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -6,8 +6,15 @@ class Pet < ApplicationRecord
 
   enum gender: [ :male, :female ]
 
-  def age
+  def age_years
     now = Time.current
     now.year - self.dob.year - ((now.month > self.dob.month || (now.month == self.dob.month && now.day >= self.dob.day)) ? 0 : 1)
+  end
+
+  def age_months
+    if age_years == 0
+      now = Time.current
+      now.month - self.dob.month - ((now.day >= self.dob.day || now.day < self.dob.day) ? 0 : 1)
+    end
   end
 end
