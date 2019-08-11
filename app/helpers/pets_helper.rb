4231b6991bedec4da6840da94ecc132ec429e24c
Irma Mejia <ijanettmejia@outlook.com>
2019-08-11 08:57:34 -0400

    Add partials to pet show page

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -3,4 +3,8 @@ module PetsHelper
     def birthdate(pet)
         pet.dob.strftime("%b %d, %Y")
     end
+
+    def upcoming_appts(pet)
+        Appointment.upcoming_appts_for_pet(pet)
+    end
 end
