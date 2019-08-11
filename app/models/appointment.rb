675717aed6159b787a9faf489005d9b54476c856
Irma Mejia <ijanettmejia@outlook.com>
2019-08-10 23:20:05 -0400

    Add scope methods that retrieve past and upcoming appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,6 +6,18 @@ class Appointment < ApplicationRecord
   validates_uniqueness_of :time, {scope: [:date, :vet_id], message: "unavailable, please choose a different time"}
 
   def self.upcoming_appts
-    where('appointments.date >= ?', DateTime.now)
+    where('appointments.date >= ?', DateTime.now).order(date: :asc).order(time: :asc)
+  end
+
+  def self.past_appts
+    where('appointments.date < ?', DateTime.now).order(date: :desc).order(time: :desc)
+  end
+
+  def self.upcoming_appts_for(vet)
+    self.upcoming_appts.where('appointments.vet_id == ?', vet.id)
+  end
+
+  def self.past_appts_for(vet)
+    self.past_appts.where('appointments.vet_id == ?', vet.id)
   end
 end
