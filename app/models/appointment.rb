7817a6f5b358e4371cfe190f93fb5ddc467bf54f
Irma Mejia <ijanettmejia@outlook.com>
2019-08-10 22:33:27 -0400

    Add scope method to appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,10 +3,9 @@ class Appointment < ApplicationRecord
   belongs_to :pet
 
   validates_presence_of :vet_id, :pet_id, :date, :time
+  validates_uniqueness_of :time, {scope: [:date, :vet_id], message: "unavailable, please choose a different time"}
 
   def self.upcoming_appts
-    where("date ")
+    where('appointments.date >= ?', DateTime.now)
   end
-
-  
 end
