05520e0843f52e7e6c60cace77821f13987a9853
Irma Mejia <ijanettmejia@outlook.com>
2019-08-11 12:11:44 -0400

    Rename pet and vet appointment helper methods

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -4,7 +4,7 @@ module PetsHelper
         pet.dob.strftime("%b %d, %Y")
     end
 
-    def upcoming_appts(pet)
+    def pet_upcoming_appts(pet)
         Appointment.upcoming_appts_for_pet(pet)
     end
 end
--- app/helpers/vets_helper.rb
+++ app/helpers/vets_helper.rb
@@ -1,2 +1,10 @@
 module VetsHelper
+
+    def vet_upcoming_appts(vet)
+        Appointment.upcoming_appts_for_vet(vet)
+    end
+
+    def vet_past_appts(vet)
+        Appointment.past_appts_for_vet(vet)
+    end
 end
